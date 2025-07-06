49f152fd6760305de1e1c88129a6e9811e4ff8d3
Urwa-Binat-Khalid <urwashanza99@gmail.com>
2025-07-06 21:16:45 +0500

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,8 @@
+# config.py
+
+DATA_DIR = "/kaggle/input/cat-sound"
+CSV_PATH = "/kaggle/working/balanced_dataset.csv"
+OUT_DIR = "/kaggle/working/specs"
+SAMPLE_RATE = 16000
+DURATION = 2.0
+NUM_CLASSES = 3  # Brushing, Isolation, WaitingForFood
